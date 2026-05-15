77da19874dd4fe3e69512ca34eac051310d7590e
u25044215-Hannah <u25044215@tuks.co.za>
2026-05-15 22:57:10 +0200

    Create configTest.php to output JSON config

---
+++ configTest.php
@@ -0,0 +1,12 @@
+<?php
+require_once "config.php";
+
+header("Content-Type: application/json");
+
+echo json_encode([
+    "DB_HOST" => DB_HOST,
+    "DB_USER" => DB_USER,
+    "DB_NAME" => DB_NAME,
+    "SERVER_API_KEY_LOADED" => SERVER_API_KEY ? true : false
+]);
+?>
