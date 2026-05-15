07ecbccc78f71bb79b0294bcf87f1413a481907d
u25044215-Hannah <u25044215@tuks.co.za>
2026-05-15 20:21:29 +0200

    ConfigEx update

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// config.php
+
+// IMPORTANT: Change these to your Wheatley database details.
+define("DB_HOST", "wheatley.cs.up.ac.za");
+define("DB_USER", "uXXXXXXXX"); 
+define("DB_PASS", "YOUR_DB_PASSWORD");
+define("DB_NAME", "uXXXXXXXX_flight_tracking");
+
+// This key is used by your NodeJS server for UpdateFlightPosition.
+// You can choose your own long random string.
+define("SERVER_API_KEY", "CHANGE_THIS_TO_A_SECRET_SERVER_KEY");
+?>
